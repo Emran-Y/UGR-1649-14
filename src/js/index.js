d3678d158eb19ab297e2ca3a0ed36e60ac7fecf9
EMRAN YONAS YIMER <emranyonas602@gmail.com>
2023-12-18 10:11:43 +0300

    Form Validation Added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 const navToggler = document.getElementById('nav-toggler');
 const nav = document.getElementById('nav');
-
+const messageOnSubmit = document.getElementById('messageOnSubmit');
 
 navToggler.addEventListener('click', () => {
     if (navToggler.classList.contains('toggle')) {
@@ -9,5 +9,26 @@ navToggler.addEventListener('click', () => {
     } else {
         navToggler.classList.add('toggle');
         nav.classList.add('nav--active');
+    }
+});
+
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.forms['myForm']['Name'].value;
+    const email = document.forms['myForm']['Email'].value;
+    const message = document.forms['myForm']['Message'].value;
+
+    if (!name) {
+        messageOnSubmit.style.color = 'red';
+        return messageOnSubmit.textContent = 'Name is required';
+    } else if (!email) {
+        messageOnSubmit.style.color = 'red';
+        return messageOnSubmit.textContent = 'Email is required';
+    } else if (!message) {
+        messageOnSubmit.style.color = 'red';
+        return messageOnSubmit.textContent = 'Message is required';
+    } else {
+        messageOnSubmit.style.color = 'green';
+        return messageOnSubmit.textContent = 'I have received your message. I will contact you soon';
     }
 });
